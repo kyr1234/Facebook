1bfd43fc24ace9d24486d3c008cf54105433c2a6
Yug KHOKHAR <83321546+kyr1234@users.noreply.github.com>
2021-08-25 14:27:08 +0000

    changed in components

--- src/Components/App_Body/Sidebar/Sidebar.js
+++ src/Components/App_Body/Sidebar/Sidebar.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import './Sidebar.js';
-
+import './Sidebar.css';
+import SideBarRow from './SideBarRow.js';
+import Avatar from '@material-ui/core/Avatar';
+import Hospital from '@material-ui/icons/LocalHospitalOutlined';
+import Flag from '@material-ui/icons/FlagOutlined';
+import Contacts from '@material-ui/icons/SupervisedUserCircle';
+import Messenger from '@material-ui/icons/MessageOutlined';
+import Store from '@material-ui/icons/Storefront';
+import Videolib from '@material-ui/icons/VideoLibraryOutlined';
+import Dropdown from '@material-ui/icons/ArrowDropDownOutlined';
 
 function Sidebar() {
   return (
-    <div className="Sidebar">
+  <div className="Sidebar">
      
-  <div className='user'></div>
-  <div className='covid'></div>
-  <div className='pages'></div>
-  <div className='user'></div>
-  <div className='user'></div>
-  <div className='user'></div>
-  <div className='user'></div>
-  <div className='user'></div>
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
+  <SideBarRow title='YUG KHOKHAR' Icon={Avatar}/>
+  <SideBarRow title='Covid-19 Information center' Icon={Hospital}/>
+  <SideBarRow title='Pages' Icon={Flag}/>
+  <SideBarRow title='Friends' Icon={Contacts}/>
+  <SideBarRow title='Messenger' Icon={Messenger}/>
+  <SideBarRow title='Marketplace' Icon={Store}/>
+  <SideBarRow title='Videos' Icon={Videolib}/>
+  <SideBarRow title='MarketPlace' Icon={Dropdown}/>
+   
+  </div>
   );
 }
 
